55a57d4a2f962392bc8a60a74c1b1e102c4cd0e0
Jery <jery@tymate.com>
2019-03-19 10:02:01 +0100

    first commit

---
+++ essaiTraTests/essaiTraTests.swift
@@ -0,0 +1,37 @@
+//
+//  essaiTraTests.swift
+//  essaiTraTests
+//
+//  Created by Jery on 19/03/2019.
+//  Copyright © 2019 Jery. All rights reserved.
+//
+
+import XCTest
+@testable import essaiTra
+
+class essaiTraTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        XCTAssert(true)
+    }
+
+//    func testExample2() {
+//        XCTAssert(false)
+//    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
